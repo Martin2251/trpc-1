378390b4c91c77a61e64afbbe722e244a0634d94
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-25 23:32:23 +0000

    app tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { withTRPC } from '@trpc/next'
+import { httpBatchLink, loggerLink } from '@trpc/client'
 
 
  function App({ Component, pageProps }: AppProps) {
@@ -8,5 +9,22 @@ import { withTRPC } from '@trpc/next'
 }
 
 export default withTRPC({
-  
+  config({ctx}) {
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL 
+    ? `https://${process.env.Next_PUBLIC_VERCEL_URL}/api/trpc`
+    : 'http://localhost:3000/api/trpc'
+
+    const links = [
+      loggerLink(),
+      httpBatchLink({
+        maxBatchSize: 10,
+        url
+      })
+    ]
+
+    return{
+      queryClientConfig
+      links
+    }
+  }
 })
